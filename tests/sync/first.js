5e973f59ad471fc1fa6d7e374d37241fd4e5b39b
Jamesernator <thejamesernator@gmail.com>
2017-11-07 01:59:09 +1300

    Added tests for first

---
+++ tests/sync/first.js
@@ -0,0 +1,44 @@
+import test from "ava"
+import first from "../../src/sync/first.mjs"
+
+test('first with no arguments returns the first element of the sequence', t => {
+    t.is(
+        first([1, 2, 3, 4]),
+        1,
+    )
+
+    t.is(
+        first(['banana', 342]),
+        'banana',
+    )
+})
+
+test('first with no arguments throws an error on an empty sequence', t => {
+    t.throws(_ => first([]))
+})
+
+
+test('first with numeric argument returns a sequence of that length', t => {
+    t.deepEqual(
+        first([1, 2, 3, 4, 5, 6, 7], 3),
+        [1, 2, 3],
+    )
+
+    t.deepEqual(
+        first([1, 2, 3, 4], 0),
+        [],
+    )
+})
+
+test('first with count that is too short throws an error', t => {
+    t.throws(_ => first([1, 2], 3))
+    t.throws(_ => first([], 1))
+    t.notThrows(_ => first([], 0))
+})
+
+test('first with count that is too short can be supressed by passing true', t => {
+    t.deepEqual(
+        first([1, 2], 3, true),
+        [1, 2],
+    )
+})
