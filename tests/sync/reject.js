3f114eee14c0bdb57e2bd63e02ad027ce3398356
Jamesernator <thejamesernator@gmail.com>
2017-11-13 00:16:57 +1300

    Reject test

---
+++ tests/sync/reject.js
@@ -0,0 +1,29 @@
+import test from "ava"
+import reject from "../../src/sync/reject.mjs"
+import toArray from "../../src/sync/toArray.mjs"
+
+test('reject basic functionality', t => {
+    const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    t.deepEqual(
+        toArray(reject(data, x => x % 3 === 0)),
+        [1, 2, 4, 5, 7, 8, 10],
+    )
+})
+
+test('reject receives correct arguments', t => {
+    const data = [4, 3, 2, 1]
+
+    t.deepEqual(
+        toArray(reject(data, (_, idx) => idx % 2 === 0)),
+        [3, 1],
+    )
+
+    reject(data, (_, __, ...rest) => t.deepEqual(rest, []))
+})
+
+test('reject throws early on bad input', t => {
+    t.throws(_ => reject([], 2))
+
+    t.throws(_ => reject([], _ => true, 'banana'))
+})
