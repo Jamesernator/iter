8fa16379a69fad34ae95abfd98537cd1fb7107ae
jamesernator <thejamesernator@gmail.com>
2017-11-12 21:57:09 +1300

    Added recursive option to replaceError

--- src/sync/replaceError.mjs
+++ src/sync/replaceError.mjs
@@ -2,13 +2,19 @@ import { raw as create } from "./createMethod.mjs"
 import { raw as iterableGenerator } from "./iterableGenerator.mjs"
 import assert from "../--assert.mjs"
 
-const _replaceError = iterableGenerator(function* replaceError(iterable, replacer) {
-    try {
-        yield* iterable
-    } catch (err) {
-        yield* replacer(err)
-    }
-})
+const _replaceError = iterableGenerator(
+    function* replaceError(iterable, replacer, recursive=false) {
+        try {
+            yield* iterable
+        } catch (err) {
+            if (recursive) {
+                yield* _replaceError(replacer(err), replacer, recursive)
+            } else {
+                yield* replacer()
+            }
+        }
+    },
+)
 
 function replaceError(iterable, replacer, ...rest) {
     assert.function(replacer, `[replaceError] Expected replacer to be a function`)
