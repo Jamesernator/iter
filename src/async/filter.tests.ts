c0843de6cbe4374224b53648de5796c37ab2aeaa
James <thejamesernator@gmail.com>
2019-07-06 00:05:45 +1000

    .

--- src/async/filter.tests.ts
+++ src/async/filter.tests.ts
@@ -1,44 +1,44 @@
-import test from "ava";
+import * as assert from "../lib/assert.js";
+import iterator from "./iterator.js";
 import filter from "./filter.js";
 import toArray from "./toArray.js";
 import CountClosing from "./helpers/CountClosing.js";
-import iterator from "./iterator.js";
 
-test("filter basic functionality", async (t) => {
-    const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+export const tests = {
+    async "filter can filter data out of the sequence"() {
+        const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        const expected = [3, 6, 9];
 
-    t.deepEqual(
-        await toArray(filter(data, (x) => x % 3 === 0)),
-        [3, 6, 9],
-    );
-});
+        assert.deepEqual(expected, await toArray(filter(data, (i) => i % 3 === 0)));
+    },
 
-test("filter receives correct arguments", async (t) => {
-    const data = [4, 3, 2, 1];
+    async "filter receives the index"() {
+        const data = [4, 3, 2, 1];
+        const expected = [4, 2];
 
-    t.deepEqual(
-        await toArray(filter(data, (_, idx) => idx % 2 === 0)),
-        [4, 2],
-    );
+        assert.deepEqual(expected, await toArray(filter(data, (_, i) => i % 2 === 0)));
+    },
 
-    await toArray(filter(data, (_, __, ...rest) => t.deepEqual(rest, [])));
-});
+    async "filter iterator closing"() {
+        const iter = new CountClosing([1, 2, 3, 4]);
+        const seq = iterator(filter(iter, (x) => x % 2 === 0));
 
-test("filter iterator closing", async (t) => {
-    const data = new CountClosing([1, 2, 3, 4]);
-    const seq = filter(data, (x) => x % 2 === 0)[Symbol.asyncIterator]();
+        await seq.next();
+        await seq.return!();
 
-    await seq.next();
-    await seq.return!();
-    t.is(data.closed, 1);
-});
+        assert.is(iter.closed, 1);
+    },
 
-test("filter iterator closing on predicate error", async (t) => {
-    const data = new CountClosing([1, 2, 3, 4]);
-    const seq = iterator(filter(data, () => {
-        throw new Error("Error");
-    }));
+    async "filter iterator closing on predicate error"() {
+        const iter = new CountClosing([1, 2, 3, 4]);
+        const seq = iterator(filter(iter, (value) => {
+            if (value === 3) {
+                throw new Error("Test");
+            }
+            return true;
+        }));
 
-    await t.throwsAsync(() => seq.next());
-    t.is(data.closed, 1);
-});
+        await assert.throwsAsync(() => toArray(seq));
+        assert.is(iter.closed, 1);
+    },
+};
