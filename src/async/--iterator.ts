05ba744b7ea889ab8f32ae450834020d363d8392
James Browning <james.browning@canva.com>
2019-07-05 11:13:12 +1000

    .

--- src/async/--iterator.ts
+++ src/async/--iterator.ts
@@ -4,9 +4,7 @@ import { AsyncOrSyncIterable } from "../AsyncOrSyncIterable.js";
  * this is a tiny utility so that calling .return is idempotent and can be
  * called multiple times while only calling .return on the source iterable
  * once
-*/
-export default async function* iterator<T>(
-    iterable: AsyncOrSyncIterable<T>,
-) {
-    yield* iterable
+ */
+export default async function* iterator<T>(iterable: AsyncOrSyncIterable<T>) {
+    yield* iterable;
 }
--- src/async/enumerate.ts
+++ src/async/enumerate.ts
@@ -1,12 +1,10 @@
-import iterableGenerator from "./iterableGenerator.js"
 import { AsyncOrSyncIterable } from "../AsyncOrSyncIterable.js";
+import iterableGenerator from "./iterableGenerator.js";
 
-export default iterableGenerator(
-    async function* enumerate<T>(iterable: AsyncOrSyncIterable<T>) {
-        let idx = 0
-        for await (const item of iterable) {
-            yield [idx, item] as const
-            idx += 1
-        }
+export default iterableGenerator(async function* enumerate<T>(iterable: AsyncOrSyncIterable<T>) {
+    let idx = 0;
+    for await (const item of iterable) {
+        yield [idx, item] as const;
+        idx += 1;
     }
-)
+});
